992cf24b71988d81707f68aee339d2ea5754fb0f
sachinthyasunirma <ssrathnavibushana@gmail.com>
2023-02-02 13:47:45 +0530

    add css to main layout

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -2,10 +2,10 @@ import Navigationbar from "@/components/navbar/Navigationbar"
 
 function MainLayout({children}) {
   return (
-    <>
+    <div className="container-md pt-5">
     <Navigationbar/>
     {children}
-    </>
+    </div>
   )
 }
 
