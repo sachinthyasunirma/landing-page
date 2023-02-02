2ad584c30649e5bc930015d453b1bcd708edee5e
sachinthyasunirma <ssrathnavibushana@gmail.com>
2023-02-02 15:02:00 +0530

    add lazy loading for the image loading

--- src/components/card/PaintingCard.jsx
+++ src/components/card/PaintingCard.jsx
@@ -1,11 +1,13 @@
 import styles from "@/styles/Card.module.css";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 
 function PaintingCard(props) {
   const { id, image_url, description, price, artist } = props;
   return (
     <div className="d-flex flex-column align-items-center align-items-sm-start mb-2 p-1">
       <div className={styles.image}>
-        <img className="img-fluid" src={image_url} alt="image" />
+        <LazyLoadImage className="img-fluid" src={image_url} alt="image" />
       </div>
       <div className="d-flex flex-wrap mt-2 justify-content-center justify-content-sm-between align-items-center w-100 gap-3 gap-sm-0">
         <div className={styles.drawing_des}>{description}</div>
